07fc65e36a16626f8076779e55e7e447fea43f1a
pinkpinkman <38169661+pinkpinkman@users.noreply.github.com>
2019-07-26 06:56:33 +0800

    Rename 2.12.6.c to C/2.12.6.c

---
+++ C/2.12.6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int toes = 10;
+    int toes2 = toes*2;
+    int toes3 = toes*toes;
+    printf("这个是toes的值：%i\n",toes);
+    printf("这个是toes两倍的值：%i\n",toes2);
+    printf("这个是toes平方的值：%i\n",toes3);
+
+    return 0;
+}
